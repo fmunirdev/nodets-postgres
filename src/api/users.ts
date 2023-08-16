b3f07076928e862d5e5fc9d4652f965be2f73312
Fahad Munir <fmunirdev@gmail.com>
2023-08-16 13:06:33 +0500

    Add users crud operations

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,11 +1,49 @@
 import { Router, Request, Response } from 'express';
 
+import { User } from '../models';
+
 const router = Router();
 
 export default () => {
-  router.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    res.json({ id: 'dsa' });
+  router.get('/', async (req: Request, res: Response) => {
+    const users = await User.findAll({ attributes: { exclude: ['password'] } });
+    res.json(users);
+  });
+
+  router.post('/', async (req: Request, res: Response) => {
+    const { name, email, password } = req.body;
+    const newUser = await User.create({ name, email, password });
+    res.status(201).json(newUser);
+  });
+
+  router.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id, {
+      attributes: { exclude: ['password'] },
+    });
+    if (!user) res.sendStatus(404);
+    else res.json(user);
+  });
+
+  router.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) res.sendStatus(404);
+    else {
+      const { name, email, password } = req.body;
+      await user.update({ name, email, password });
+      res.json(user);
+    }
+  });
+
+  router.delete('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) res.sendStatus(404);
+    else {
+      await user?.destroy();
+      res.sendStatus(204);
+    }
   });
 
   return router;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,51 @@
-import { DataTypes } from 'sequelize';
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import bcrypt from 'bcrypt';
 import sequelize from '../db';
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.UUIDV4,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
+const SALT_ROUNDS = 12;
+
+interface UserModel
+  extends Model<
+    InferAttributes<UserModel>,
+    InferCreationAttributes<UserModel>
+  > {
+  id: CreationOptional<string>;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const User = sequelize.define<UserModel>(
+  'User',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
   },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-});
+  {
+    hooks: {
+      async beforeSave(user) {
+        user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+      },
+    },
+  },
+);
+
+User.prototype.comparePassowrd = async function (candidate: string) {
+  return await bcrypt.compare(candidate, this.password);
+};
+
+User.sync();
 
 export default User;
